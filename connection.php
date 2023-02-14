cb407a01ef15c908462957dd3bf294505d313f27
Gio4953 <125308045+Gio4953@users.noreply.github.com>
2023-02-14 15:37:37 +0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 // Connection settings
 $host = "localhost";
-$username = "sqluser";
-$password = "password";
-$dbname = "mybase";
+$username = "id20299988_sqluser";
+$password = "c&P2HD^aa0Ld\C6[";
+$dbname = "id20299988_mybase";
 
 // Create a new connection
 $conn = new mysqli($host, $username, $password, $dbname);
